9d8fb39aa5220cf360f26d1f86c0bd17a1d1d811
abdullahahtesham <146721363+abdullahahtesham@users.noreply.github.com>
2024-05-28 20:06:39 +0500

    add this

---
+++ main.js
@@ -0,0 +1,32 @@
+var generalInformation = {
+  name: "Abdullah",
+  cnicNo: 123456789,
+  age: 18,
+};
+var student1 = {
+  name: "Ubaid",
+  age: 18,
+  cnicNo: 1233333333,
+};
+var student2 = {
+  name: "Wahab",
+  age: 20,
+  cnicNo: 1221212112,
+};
+var rollNo;
+rollNo = "PIAIC";
+rollNo = 123;
+var sudent3 = {
+  name: "string",
+  rollNo: 111,
+};
+var teacher1 = {
+  name: "Ubaid",
+  eperaince: 3,
+};
+var teacher2 = {
+  name: "okasha",
+  rollNo: 12343,
+  experiance: 3,
+};
+console.log(teacher2);
